588f34e47fd3ea07d659b2969ad576faa4436d83
am-MongoDB <andrew.morgan@mongodb.com>
2021-02-02 11:15:47 +0000

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Andrew Morgan on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
